a226c8377a0d0090c3d076eb5d8d4daea08ba041
Saptarshi Basu <sapb2004@gmail.com>
2017-08-09 23:33:43 +0530

    delay reduced

--- src/js/main.js
+++ src/js/main.js
@@ -37,7 +37,7 @@ $(window).scroll(function(){
 });
 
 $(function() {
-	$(".vertical-line").fadeOut(2000, function() {
+	$(".vertical-line").fadeOut(1000, function() {
         $(".navbar-fixed, .homepage, section").fadeIn(1000);        
 		$('.scrollbutton, .aboutmelink, .brand-logo, .square1, .square2').on('click', function(e) {
 			e.preventDefault();
